0cfee3b36cbc4c2586794f64e6f38c2ff16dbfd4
Tim Fuchs <75135582+FixFaxt@users.noreply.github.com>
2024-09-07 01:26:38 +0200

    AirBnb Controller & Service

--- packages/server/src/api/controllers/airbnb.controller.ts
+++ packages/server/src/api/controllers/airbnb.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UUID } from 'crypto';
 import { AirBnbService } from 'src/features/object/airbnb/airbnb.service';
+import { AirBnbDto, UpdateAirBnbDto } from '../dto/airbnb.dto';
 
 @ApiTags('AirBnb')
 @Controller('airbnb')
@@ -11,5 +13,33 @@ export class AirBnbController {
   @ApiResponse({ status: 200, description: 'Get All AirBnbs and the count' })
   getAllAirBnbs() {
     return this.airbnbService.getAllAirBnbs();
+  }
+
+  @Get(':id')
+  @ApiResponse({ status: 200, description: 'Returns AirBnb by Id' })
+  getAirBnbById(@Param('id') id: UUID) {
+    return this.airbnbService.getAirBnbById(id);
+  }
+
+  @Post()
+  @ApiResponse({ status: 201, description: 'Creates a new AirBnb' })
+  createAirBnb(@Body() body: AirBnbDto) {
+    return this.airbnbService.createAirBnb(body);
+  }
+
+  @Post(':id/update')
+  @ApiResponse({
+    status: 200,
+    description: 'Update an AirBnb by Id',
+  })
+  @ApiBody({ type: UpdateAirBnbDto })
+  updateAirBnb(@Param('id') id: UUID, @Body() body: UpdateAirBnbDto) {
+    return this.airbnbService.updateAirBnb(id, body);
+  }
+
+  @Delete(':id')
+  @ApiResponse({ status: 200, description: 'Delete an AirBnb by Id' })
+  deleteAirBnb(@Param('id') id: UUID) {
+    return this.airbnbService.deleteAirBnb(id);
   }
 }
--- packages/server/src/features/object/airbnb/airbnb.service.ts
+++ packages/server/src/features/object/airbnb/airbnb.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UUID } from 'crypto';
+import { AirBnbDto, UpdateAirBnbDto } from 'src/api/dto/airbnb.dto';
 import { AirBnb } from 'src/entities/airbnb.entity';
 import { Repository } from 'typeorm';
 
@@ -12,5 +14,46 @@ export class AirBnbService {
 
   async getAllAirBnbs() {
     return await this.airbnbRepository.findAndCount();
+  }
+
+  async getAirBnbById(id: UUID) {
+    return await this.airbnbRepository.findOneBy({ id: id });
+  }
+
+  async createAirBnb(data: AirBnbDto) {
+    return await this.airbnbRepository.save({
+      name: data.name,
+      description: data.description,
+      owners: data.owners,
+      active: data.active,
+      rating: data.rating,
+      address: { id: data.addressId },
+      hostName: data.hostName,
+      bedrooms: data.bedrooms,
+      bathrooms: data.bathrooms,
+      isShared: data.isShared,
+    });
+  }
+
+  async updateAirBnb(id: UUID, data: UpdateAirBnbDto) {
+    return await this.airbnbRepository.update(
+      { id: id },
+      {
+        name: data.name,
+        description: data.description,
+        owners: data.owners,
+        active: data.active,
+        rating: data.rating,
+        address: { id: data.addressId },
+        hostName: data.hostName,
+        bedrooms: data.bedrooms,
+        bathrooms: data.bathrooms,
+        isShared: data.isShared,
+      },
+    );
+  }
+
+  async deleteAirBnb(id: UUID) {
+    return await this.airbnbRepository.delete({ id: id });
   }
 }
