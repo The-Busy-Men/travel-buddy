dbcf21cf9e32983036104f7012e1f934804f275e
Tim Fuchs <75135582+FixFaxt@users.noreply.github.com>
2024-09-13 01:17:22 +0200

    add tailwindcss

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,16 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
-// import { Test } from './pages/test/test-display';
 import AppRoutes from './routes';
-
+import './style.css'
+import React from 'react';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <>
+    <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <AppRoutes />
       </QueryClientProvider>
+    </React.StrictMode>
     </>
   );
 }
