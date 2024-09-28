e31b57fe3b144613b86fb76ba21661bedcc55bd6
Tim Fuchs <75135582+FixFaxt@users.noreply.github.com>
2024-09-28 23:11:47 +0200

    New Routes

--- packages/web/src/routes.tsx
+++ packages/web/src/routes.tsx
@@ -42,18 +42,58 @@ const AppRoutes = () => {
     <Router>
       <Header />
       <Routes>
+        {/* General */}
         <Route path="/" element={<Landing />} />
-        <Route path="/test" element={<Test />} />
+        <Route path='/about' element={undefined} />
+        <Route path='/help' element={undefined} />
+        <Route path='/tos' element={undefined} />
+        <Route path='/privacy' element={undefined} />
+
+        {/* Admin Routes */}
         <Route path='/admin' element={<AdminMainPage />} />
-        <Route path="/admin/hotels/:hotelId" element={<HotelAdminPageWrapper />} />
-        <Route path="/admin/hotels" element={<AllObjectsPage objectType='hotel' />} />
-        <Route path="/admin/airbnbs/:airbnbId" element={<AirBnbAdminPageWrapper />} />
-        <Route path="/admin/airbnbs" element={<AllObjectsPage objectType='airbnb' />} />
-        <Route path="/hotels/:hotelId" element={<HotelUserPageWrapper />} />
-        <Route path="/airbnbs/:airbnbId" element={<AirBnbUserPageWrapper />} />
-        <Route path="/admin/groups" element={<GroupList />} />
+          {/* Superadmin */}
+          <Route path='/admin/settings' element={undefined} />
+          <Route path='/admin/users' element={undefined} />
+          <Route path='/admin/users/:userId' element={undefined} />
+
+          {/* Admin */}
+          <Route path="/admin/hotels" element={<AllObjectsPage objectType='hotel' />} />
+          <Route path="/admin/hotels/:hotelId" element={<HotelAdminPageWrapper />} />
+          <Route path="/admin/airbnbs" element={<AllObjectsPage objectType='airbnb' />} />
+          <Route path="/admin/airbnbs/:airbnbId" element={<AirBnbAdminPageWrapper />} />
+          <Route path="/admin/groups" element={<GroupList />} />
+          <Route path='/admin/groups/:groupId' element={undefined} />
+          <Route path='/admin/analytics' element={undefined} />
+          <Route path='/admin/notifications' element={undefined} />
+
+          {/* Moderator */}
+          <Route path='/admin/reports' element={undefined} />
+          <Route path='/admin/chats' element={undefined} />
+
+        {/* User Routes */}
+          {/* Objects */}
+          <Route path='/hotels' element={undefined} />
+          <Route path="/hotels/:hotelId" element={<HotelUserPageWrapper />} />
+          
+          <Route path='/airbnbs' element={undefined} />
+          <Route path="/airbnbs/:airbnbId" element={<AirBnbUserPageWrapper />} />
+          
+          <Route path='/object/create' element={undefined} />
+        
+          {/* User specific */}
+          <Route path='/groups' element={undefined} />
+          <Route path='/groups/:groupId' element={undefined} />
+          <Route path='/profile' element={undefined} />
+
+          {/* Other */}
+          <Route path='/destinations' element={undefined} />
+          <Route path="/b/login" element={<LoginBusiness />} />
+
+        {/* Testing Routes */}
+        <Route path="/test" element={<Test />} />
         <Route path='/object/test' element={<ObjectCreateForm />} />
-        <Route path="/b/login" element={<LoginBusiness />} />
+
+        {/* Error Routes */}
         <Route path="*" element={<NotFound />} />
       </Routes>
       {!isAdminRoute && <Footer />}
