92e9b3e3fbe6b12fd09d86bef71c670baf4006dd
Tim Fuchs <75135582+FixFaxt@users.noreply.github.com>
2024-11-02 18:36:12 +0100

    Buttons for appr,deny,back to pending in preview page

--- packages/web/src/pages/approvals/user/previewObject.tsx
+++ packages/web/src/pages/approvals/user/previewObject.tsx
@@ -4,6 +4,11 @@ import WarningWrapper from "../../components/wrappers/warningWrapper";
 import { useApprovalById } from "../../forms/hooks/useApprovalById";
 import UserObjectDetails from "../../objects/users/objectDetails";
 import { Loader } from "../../components/ui/loader";
+import { Button } from "../../components/ui/button";
+import { Card, CardContent, CardHeader } from "../../components/ui/card";
+import { CiSquareCheck } from "react-icons/ci";
+import { CiSquareQuestion } from "react-icons/ci";
+import { CiSquareRemove } from "react-icons/ci";
 
 const PreviewObjectPage = ({approvalId}: { approvalId: UUID;}) => {
   const {approvalData, approvalDataIsLoading} = useApprovalById(approvalId);
@@ -12,6 +17,17 @@ const PreviewObjectPage = ({approvalId}: { approvalId: UUID;}) => {
   return (
     <>
       <WarningWrapper message={`Preview for Approval Request: ${approvalId}`}>
+        <Card>
+          <CardHeader>This request is currently: {approvalData?.status.charAt(0).toUpperCase() + approvalData?.status.slice(1)}</CardHeader>
+          <CardContent>
+            <div className="flex flex-row">
+              <Button variant='ghost' className="text-green-500 hover:bg-green-500 hover:bg-opacity-20 mr-2" disabled={approvalData?.status === 'approved'}><CiSquareCheck className="mr-2" size={20}/>Approve Request</Button>
+              <Button variant='ghost' className="text-orange-500 hover:bg-orange-500 hover:bg-opacity-20 mr-2" disabled={approvalData?.status === 'pending'}><CiSquareQuestion className="mr-2" size={20}/>Revert to Pending</Button>
+              <Button variant='ghost' className="text-red-500 hover:bg-red-500 hover:bg-opacity-20" disabled={approvalData?.status === 'rejected'}><CiSquareRemove className="mr-2" size={20}/>Reject Request</Button>
+            </div>
+          </CardContent>
+        </Card>
+
         <Loader show={approvalDataIsLoading} fullscreen />
         {!approvalDataIsLoading &&  <UserObjectDetails objectData={dataAsObject} objectType={approvalData?.type} /> }
       </WarningWrapper>
